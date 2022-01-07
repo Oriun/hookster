2094173f2229cafc48cfcd3d708d42aff1747068
Oriun <71255433+Oriun@users.noreply.github.com>
2022-01-07 19:36:04 +0100

    refactor repo

--- index.js
+++ index.js
@@ -6,8 +6,11 @@
  * 
  * 
  */
-exports.useDiffState = require('./src/useDiffState')
-exports.useMedia = require('./src/useMedia')
-exports.useRouting = require('./src/useRouting')
-exports.useWEvent = require('./src/useWEvent')
-exports.useStorage = require('./src/useStorage')
+export { default as useMedia } from '@oriun/useMedia'
+export { default as useWEvent } from '@oriun/useWEvent'
+export { default as useRouting } from '@oriun/useRouting'
+export { default as useStorage } from '@oriun/useStorage'
+export { default as useFeedback } from '@oriun/useFeedback'
+export { default as useDiffState } from '@oriun/useDiffState'
+export { default as useClickOutside } from '@oriun/useClickOutside'
+export { default as useErrorSnapshot } from '@oriun/useErrorSnapshot'
--- src/useErrorSnapshot.js
+++
@@ -1,9 +0,0 @@
-const { useRef, useState } = require('react')
-
-const useErrorSnapshot = ({
-    mode
-}) => {
-
-}
-
-module.exports = useErrorSnapshot
