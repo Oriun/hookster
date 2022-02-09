745bbaa77bdce78b0b572edbe677ee22cef67d17
Oriun <71255433+Oriun@users.noreply.github.com>
2022-02-09 18:05:49 +0100

    fix type declaration

--- lib/useMedia/useMedia.d.ts
+++ lib/useMedia/useMedia.d.ts
@@ -2,4 +2,4 @@ import React from "react";
 
 export default function <T extends { [key: string]: string | number }>(
   queries: T
-): [{ [key in keyof T]: boolean }, React.Ref<HTMLElement>];
+): [{ [key in keyof T]: boolean }, React.RefObject<any>];
