0693298a21edcf0c4583eb2234f8e565a34112c2
Oriun <emmanuel.nuiro@epitech.eu>
2022-09-13 11:27:16 +0200

    update

--- lib/useMedia/useMedia.d.ts
+++ lib/useMedia/useMedia.d.ts
@@ -2,6 +2,7 @@ import React from "react";
 
 export type Query = { [key: string]: string | number };
 
-export default function <T extends Query, S extends HTMLElement = HTMLElement>(
-  queries: T
-): [{ [key in keyof T]: boolean }, React.RefObject<S>];
+export default function <
+  S extends HTMLElement = HTMLElement,
+  T extends Query = Query
+>(queries: T): [{ [key in keyof T]: boolean }, React.RefObject<S>];
